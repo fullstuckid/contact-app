8c68bb6efdfc97bf4fd6debfb8c2452cf4e1846b
infinitedim <saputradimas.boediluhur@gmail.com>
2022-10-24 09:12:30 +0700

    types version

--- app.js
+++ app.js
@@ -1,71 +1,67 @@
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
-import {
-  saveContact,
-  listContact,
-  detailContact,
-  deleteContact,
-} from "./contacts.js";
-
-yargs(hideBin(process.argv))
-  .command({
+"use strict";
+exports.__esModule = true;
+var yargs_1 = require("yargs");
+var helpers_1 = require("yargs/helpers");
+var contacts_js_1 = require("./contacts.js");
+(0, yargs_1["default"])((0, helpers_1.hideBin)(process.argv))
+    .command({
     command: "add",
     describe: "Menambahkan kontak baru",
     builder: {
-      nama: {
-        describe: "Nama",
-        demandOption: true,
-        type: "string",
-      },
-      email: {
-        describe: "Email",
-        demandOption: false,
-        type: "string",
-      },
-      noHP: {
-        describe: "No HP",
-        demandOption: true,
-        type: "string",
-      },
+        nama: {
+            describe: "Nama",
+            demandOption: true,
+            type: "string"
+        },
+        email: {
+            describe: "Email",
+            demandOption: false,
+            type: "string"
+        },
+        noHP: {
+            describe: "No HP",
+            demandOption: true,
+            type: "string"
+        }
     },
-    handler(argv) {
-      saveContact(argv.nama, argv.email, argv.noHP);
-    },
-  })
-  .command({
+    handler: function (argv) {
+        (0, contacts_js_1.saveContact)(argv.nama, argv.email, argv.noHP);
+    }
+})
+    .command({
     command: "list",
     describe: "Menampilkan semua isi kontak",
-    handler() {
-      listContact();
-    },
-  })
-  .command({
+    handler: function () {
+        (0, contacts_js_1.listContact)();
+    }
+})
+    .command({
     command: "detail",
     describe: "Menampilkan detail kontak berdasarkan nama",
     builder: {
-      nama: {
-        describe: "Nama",
-        demandOption: true,
-        type: "string",
-      },
+        nama: {
+            describe: "Nama",
+            demandOption: true,
+            type: "string"
+        }
     },
-    handler(argv) {
-      detailContact(argv.nama);
-    },
-  })
-  .command({
+    handler: function (argv) {
+        (0, contacts_js_1.detailContact)(argv.nama);
+    }
+})
+    .command({
     command: "delete",
     describe: "Menghapus kontak berdasarkan nama",
     builder: {
-      nama: {
-        describe: "Nama",
-        demandOption: true,
-        type: "string",
-      },
+        nama: {
+            describe: "Nama",
+            demandOption: true,
+            type: "string"
+        }
     },
-    handler(argv) {
-      deleteContact(argv.nama);
-    },
-  })
-  .demandCommand()
-  .parse();
+    handler: function (argv) {
+        (0, contacts_js_1.deleteContact)(argv.nama);
+    }
+})
+    .demandCommand()
+    .parse();
--- contacts.js
+++ contacts.js
@@ -1,101 +1,84 @@
-import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
-import validator from "validator";
-
-const path = "./data";
-
-if (!existsSync(path)) {
-  mkdirSync(path);
+"use strict";
+exports.__esModule = true;
+exports.deleteContact = exports.detailContact = exports.listContact = exports.saveContact = void 0;
+var fs_1 = require("fs");
+var validator_1 = require("validator");
+var path = "./data";
+if (!(0, fs_1.existsSync)(path)) {
+    (0, fs_1.mkdirSync)(path);
 }
-
-const file = "./data/contacts.json";
-
-if (!existsSync(file)) {
-  writeFileSync(file, "[]", "utf-8");
+var file = "./data/contacts.json";
+if (!(0, fs_1.existsSync)(file)) {
+    (0, fs_1.writeFileSync)(file, "[]", "utf-8");
 }
-
-const loadContact = () => {
-  const files = readFileSync(file, "utf-8");
-  const contacts = JSON.parse(files);
-  return contacts;
+var loadContact = function () {
+    var files = (0, fs_1.readFileSync)(file, "utf-8");
+    var contacts = JSON.parse(files);
+    return contacts;
 };
-
-const saveContact = (nama, email, noHP) => {
-  const contact = {
-    nama,
-    email,
-    noHP,
-  };
-
-  const contacts = loadContact();
-
-  const duplikat = contacts.find((kontak) => kontak.nama === nama);
-
-  if (duplikat) {
-    console.error("Kontak sudah terdaftar, gunakan nama lain");
-    return false;
-  }
-
-  if (email) {
-    if (!validator.isEmail(email)) {
-      console.error("Format email yang anda gunakan tidak valid");
-      return false;
+var saveContact = function (nama, email, noHP) {
+    var contact = {
+        nama: nama,
+        email: email,
+        noHP: noHP
+    };
+    var contacts = loadContact();
+    var duplikat = contacts.find(function (kontak) { return kontak.nama === nama; });
+    if (duplikat) {
+        console.error("Kontak sudah terdaftar, gunakan nama lain");
+        return false;
     }
-  }
-
-  if (!validator.isMobilePhone(noHP, "id-ID")) {
-    console.error("Masukan nomor dengan benar");
-    return false;
-  }
-
-  contacts.push(contact);
-
-  writeFileSync(file, JSON.stringify(contacts));
-
-  console.info("Terimakasih sudah memasukan todo.");
-};
-
-const listContact = () => {
-  console.log("Daftar kontak");
-  const contacts = loadContact();
-
-  contacts.forEach((contact, i) => {
-    console.log(`${i + 1}. ${contact.nama} - ${contact.noHP}`);
-  });
-};
-
-const detailContact = (nama) => {
-  const contacts = loadContact();
-
-  const contact = contacts.find(
-    (kontak) => kontak.nama.toLowerCase() === nama.toLowerCase()
-  );
-
-  if (contact) {
-    console.log(contact.nama);
-    if (contact.email) {
-      console.log(contact.email);
+    if (email) {
+        if (!validator_1["default"].isEmail(email)) {
+            console.error("Format email yang anda gunakan tidak valid");
+            return false;
+        }
     }
-    console.log(contact.noHP);
-  } else {
-    console.error("Ups kontak yang anda cari tidak ada");
-    return false;
-  }
+    if (!validator_1["default"].isMobilePhone(noHP, "id-ID")) {
+        console.error("Masukan nomor dengan benar");
+        return false;
+    }
+    contacts.push(contact);
+    (0, fs_1.writeFileSync)(file, JSON.stringify(contacts));
+    console.info("Terimakasih sudah memasukan data.");
 };
-
-const deleteContact = (nama) => {
-  const contacts = loadContact();
-
-  const newContact = contacts.filter(
-    (contact) => contact.nama.toLowerCase() !== nama.toLowerCase()
-  );
-
-  if (contacts.legth === newContact.length) {
-    console.error("Kontak yang ingin anda hapus tidak ada");
-    return false;
-  }
-
-  writeFileSync(file, JSON.stringify(newContact));
-  console.log("Kontak sudah terhapus");
+exports.saveContact = saveContact;
+var listContact = function () {
+    console.log("Daftar kontak");
+    var contacts = loadContact();
+    contacts.forEach(function (contact, i) {
+        console.log("".concat(i + 1, ". ").concat(contact.nama, " - ").concat(contact.noHP));
+    });
 };
-
-export { saveContact, listContact, detailContact, deleteContact };
+exports.listContact = listContact;
+var detailContact = function (nama) {
+    var contacts = loadContact();
+    var contact = contacts.find(function (kontak) {
+        return kontak.nama.toLowerCase() === nama.toLowerCase();
+    });
+    if (contact) {
+        console.log(contact.nama);
+        if (contact.email) {
+            console.log(contact.email);
+        }
+        console.log(contact.noHP);
+    }
+    else {
+        console.error("Ups kontak yang anda cari tidak ada");
+        return false;
+    }
+};
+exports.detailContact = detailContact;
+var deleteContact = function (nama) {
+    var contacts = loadContact();
+    var newContact = contacts.filter(function (contact) {
+        return contact.nama.toLowerCase() !== nama.toLowerCase();
+    });
+    if (contacts.legth === newContact.length) {
+        console.error("Kontak yang ingin anda hapus tidak ada");
+        return false;
+    }
+    (0, fs_1.writeFileSync)(file, JSON.stringify(newContact));
+    console.log("Kontak sudah terhapus");
+};
+exports.deleteContact = deleteContact;
